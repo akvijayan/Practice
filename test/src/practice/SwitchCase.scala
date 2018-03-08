2c8faf9c199ab185a3be3801e945f408dc7cf3f1
akhilvijayan05 <akhilvijayan05@gmail.com>
2018-03-08 18:40:32 +0530

    Added Switch Case

--- test/src/practice/SwitchCase.scala
+++ test/src/practice/SwitchCase.scala
@@ -8,7 +8,7 @@ class SwitchCase {
   val i =1
   val Two = 2
 
-//warning as thsi switch case is not either tableswitch nor lookupswitch
+//warning as this switch case is not either tableswitch nor lookupswitch
   val x = (i: @switch) match {
     case 1 => "One"
     case Two => "Two"
